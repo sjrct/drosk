626f76609aeee48d3f2614e440c972e040f74e19
Chris Harding <hardinc@clarkson.edu>
2014-12-16 19:55:35 -0500

    Autodep resolution for nasm rules.

--- boot/main.pl
+++ boot/main.pl
@@ -1,13 +1,3 @@
 boot(T) :-
-	prefix('boot/src/', [
-		'leftovers.inc',
-		'before/error16.inc',
-		'before/read.inc',
-		'after/a20.inc',
-		'after/detect.inc',
-		'after/fs.inc',
-		'after/gdt.inc',
-		'after/graphics.inc'
-	], Extra),
-	nasm(T, 'boot/boot.bin', 'boot/src/boot.asm', '-fbin -Iinclude/boot/ -Iboot/src/', Extra).
+	nasm(T, 'boot/boot.bin', 'boot/src/boot.asm', ['-fbin', '-Iinclude/boot/', '-Iboot/src/']).
 
--- build.pl
+++ build.pl
@@ -8,42 +8,59 @@
 %
 
 :- use_module(library(filesex)).
+:- use_module(library(http/dcg_basics)).
 :- [boot/main, kernel/main, util/bfsgen/main].
 
 targets([boot, kernel, bfsgen]).
 
 % The top level target predicates
-build :- targets(Ts), map(build, Ts), writeln('ok.').
-clean :- targets(Ts), map(clean, Ts), writeln('ok.').
+build :- targets(Ts), map(build, Ts), writeln('ok.'), !.
+build :- writeln('not ok :(').
+clean :- targets(Ts), map(clean, Ts), writeln('ok.'), !.
+clean :- writeln('not ok :(').
 
 % The predicates for target operations, do dep check first
-build(T) :-
-	\+ call(T, build_dep).
-build(T) :-
-	call(T, build_dep),
-	lwrite(['building ', T]),
-	call(T, build).
+build(T) :- \+ call(T, build_dep), !.
+build(T) :- call(T, build_dep), lwrite(['building ', T]), call(T, build), !.
+build(T) :-	lwrite(['failed to build ', T]), false.
 
-clean(T) :-
-	\+ call(T, clean_dep).
-clean(T) :-
-	call(T, clean_dep),
-	lwrite(['cleaning ', T]),
-	call(T, clean).
+clean(T) :- \+ call(T, clean_dep), !.
+clean(T) :- call(T, clean_dep), lwrite(['cleaning ', T]), call(T, clean), !.
+clean(T) :- lwrite(['failed to clean ', T]), false.
+
+% DCG predicates for phrasing out the makefile dependencies into lists
+phr_ign --> (":" ; "\n" ; "\\"), phr_ign.
+phr_ign --> [].
+
+phr_dep(Cs) --> string(Cs), (" " ; "\n"), !.
+
+phr_deps([D | Ds]) --> phr_ign, phr_dep(D), phr_deps(Ds), !.
+phr_deps([])       --> [].
+
+% Give this predicate a stream and it will phrase out a dependcy list
+dep_list(S, D) :-
+	read_stream_to_codes(S, Codes),
+	phrase(phr_deps(B), Codes, []),
+	filter(B, [[]], C),
+	map(atom_codes, D, C).
 
 % The rule used to build NASM assembly files
-nasm(build_dep, Out, In, _, Extra) :-
-	remake3(Out, In, Extra).
+nasm(build_dep, Out, In, XArgs) :-
+	append(XArgs, [In, '-M'], Args),
+	process_create(path(nasm), Args, [stdout(pipe(S))]),
+	dep_list(S, D),
+	remake3(Out, In, D).
 
-nasm(build, Out, In, Args, _) :-
+nasm(build, Out, In, XArgs) :-
 	lwrite(['  nasm ', Out]),
-	lshell(['nasm -o ', Out, ' ', In, ' ', Args]).
+	append(['-o', Out, In], XArgs, Args),
+	process_do(nasm, Args).
 
-nasm(clean_dep, Out, _, _, _) :-
+nasm(clean_dep, Out, _, _) :-
 	exists_file(Out).
 
-nasm(clean, Out, _, _, _) :-
-	lwrite([' rm ', Out]),
+nasm(clean, Out, _, _) :-
+	lwrite(['  rm ', Out]),
 	rm(Out).
 
 % The rule used to build C files
@@ -58,14 +75,28 @@ cc(clean_dep, Out, _, _, _) :-
 	exists_file(Out).
 
 cc(clean, Out, _, _, _) :-
-	lwrite([' rm ', Out]),
+	lwrite(['  rm ', Out]),
 	rm(Out).
+
+% filters out stuff in the given list from an list
+filter([], _, []).
+filter([X | Xs], Filter, [X | Ys]) :-
+	\+ member(X, Filter),
+	filter(Xs, Filter, Ys).
+filter([X | Xs], Filter, Ys) :-
+	member(X, Filter),
+	filter(Xs, Filter, Ys).
 
 % calls a predicate for every element in a list
 map(_, []).
 map(F, [X | Xs]) :-
 	call(F, X),
 	map(F, Xs).
+
+map(_, [], []).
+map(F, [X | Xs], [Y | Ys]) :-
+	call(F, X, Y),
+	map(F, Xs, Ys).
 
 % Adds a prefix to each string in the list
 prefix(_, [], []).
@@ -94,6 +125,12 @@ lwrite(Ls) :-
 lshell(Ls) :-
 	concat(Ls, Cmd),
 	shell(Cmd).
+
+% Wraps to process_create, but returns false if status is non-0, rather than throw an error
+% Also calls alias on process name
+process_do(Name, Args) :-
+	process_create(path(Name), Args, [process(Pid)]),
+	process_wait(Pid, exit(0)).
 
 % This will return true if What needs to be remade with respect to the given file list
 remake(Obj, _) :-
